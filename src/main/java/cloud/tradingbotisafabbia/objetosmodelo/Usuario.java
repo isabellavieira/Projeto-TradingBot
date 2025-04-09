718fc376da5589e5710dad712fad456a998c8e43
Isabella <isabellavieiraaa90@gmail.com>
2025-04-09 18:29:07 -0300

    correto

--- src/main/java/cloud/tradingbotisafabbia/objetosmodelo/Usuario.java
+++ src/main/java/cloud/tradingbotisafabbia/objetosmodelo/Usuario.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class Usuario {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy     = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(nullable = false)
